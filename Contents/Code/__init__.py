3e7256540d2ac7262954ced8089f6c4d2097bbf5
sander1 <sander@plexapp.com>
2012-01-15 04:41:42 +0100

    Updated to use the HT Backdrop API

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,45 +1,41 @@
-import unicodedata
-
-HTBD_BASEURL = 'http://htbackdrops.com/'
-HTBD_SEARCH_MUSIC_ART  = HTBD_BASEURL + 'search.php?search_keywords=%s&search_terms=all&cat_id=1&search_fields=all'
-HTBD_SEARCH_MUSIC_THUMBS  = HTBD_BASEURL + 'search.php?search_keywords=%s&search_terms=all&cat_id=5&search_fields=all'
+SEARCH_ARTIST  = 'http://htbackdrops.com/api/TESTKEY/searchXML?keywords=%s&default_operator=and&limit=50&aid=1,5'
+MUSIC_ART      = 'http://htbackdrops.com/api/TESTKEY/searchXML?keywords=%s&default_operator=and&limit=50&aid=1'
+MUSIC_THUMBS   = 'http://htbackdrops.com/api/TESTKEY/searchXML?keywords=%s&default_operator=and&limit=50&aid=5'
+THUMB_URL      = 'http://htbackdrops.com/api/TESTKEY/download/%s/thumbnail' # (max. width and height of 125px)
+FULL_IMAGE_URL = 'http://htbackdrops.com/api/TESTKEY/download/%s/fullsize'  # (original full-size image)
 
 def Start():
-  HTTP.CacheTime = CACHE_1DAY
-  #adjust images per page
-  #x = str(HTTP.Request(HTBD_BASEURL + 'search.php', values={'setperpage':'30','submit':'Go'}))
-  
+  HTTP.CacheTime = CACHE_1WEEK
+
 class HTBDAgent(Agent.Artist):
   name = 'Home Theater Backdrops'
   languages = [Locale.Language.English]
   primary_provider = False
   contributes_to = ['com.plexapp.agents.lastfm']
-  
+
   def search(self, results, media, lang):
     if media.primary_metadata is not None:
-      query = String.URLEncode(String.StripDiacritics(media.primary_metadata.title))
-      Log(query)
-      #try:
+      artistName = String.StripDiacritics(media.primary_metadata.title).lower()
       previousName = ''
-      for img in HTML.ElementFromURL(HTBD_SEARCH_MUSIC_ART % query).xpath('//img[contains(@src,"./data/thumbnails")]'):
-        Log(HTML.StringFromElement(img))
-        curName = img.get('alt')
+
+      for artist in XML.ElementFromURL(SEARCH_ARTIST % String.URLEncode(artistName)).xpath('//image/title/text()'):
+        curName = String.StripDiacritics(artist).lower()
         if curName != previousName:
-          score = 100 - Util.LevenshteinDistance(media.primary_metadata.title.lower(), curName.lower())
+          score = 100 - Util.LevenshteinDistance(artistName, curName)
           results.Append(MetadataSearchResult(id = curName, score = score))
-      #except:
+          previousName = curName
+
     results.Sort('score', descending=True)
-    
+
   def update(self, metadata, media, lang):
-    for s in [HTBD_SEARCH_MUSIC_ART, HTBD_SEARCH_MUSIC_THUMBS]:
-      i = 0
-      for img in HTML.ElementFromURL(s % String.URLEncode(String.StripDiacritics(metadata.id))).xpath('//img[contains(@src,"./data/thumbnails")]'):
-        if String.StripDiacritics(img.get('alt')).lower() == String.StripDiacritics(metadata.id).lower():
-          i += 1
-          thumbUrl = img.get('src').replace('./',HTBD_BASEURL)
-          thumb = HTTP.Request(thumbUrl)
-          largeImgUrl = thumbUrl.replace('thumbnails','media')
-          if s == HTBD_SEARCH_MUSIC_ART:
-            metadata.art[largeImgUrl] = Proxy.Preview(thumb, sort_order = i)
-          else:
-            metadata.posters[largeImgUrl] = Proxy.Preview(thumb, sort_order = i)
+    for s in [MUSIC_ART, MUSIC_THUMBS]:
+      for id in XML.ElementFromURL(s % String.URLEncode(metadata.id)).xpath('//image/id/text()'):
+        thumb = HTTP.Request(THUMB_URL % (id), cacheTime=CACHE_1MONTH)
+        largeImgUrl = FULL_IMAGE_URL % id
+
+        if s == MUSIC_ART:
+          if largeImgUrl not in metadata.art:
+            metadata.art[largeImgUrl] = Proxy.Preview(thumb)
+        else:
+          if largeImgUrl not in metadata.posters:
+            metadata.posters[largeImgUrl] = Proxy.Preview(thumb)
